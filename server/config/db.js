fb64c7ff6a428af06cbe1a4e337465f3cf7445c0
VinayM6 <vinaymannem97@gmail.com>
2021-10-27 22:11:26 -0400

    first commit- 27

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/book_store"
+  "URI":"mongodb+srv://vinay:e4YFF7OhOrTSWsSI@mongodbserver.hmapq.mongodb.net/My_DataBase?retryWrites=true&w=majority"
+};
